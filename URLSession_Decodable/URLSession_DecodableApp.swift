efe73d5290aaa91daa375de49ab26bd9dc6d475c
Iclementem <iclementem@outlook.es>
2023-02-01 09:48:04 +0100

    Initial Commit

---
+++ URLSession_Decodable/URLSession_DecodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_DecodableApp.swift
+//  URLSession_Decodable
+//
+//  Created by Iván Clemente Moreno on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_DecodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
